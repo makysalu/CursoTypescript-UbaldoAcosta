83bc1d03a0aa0f029009960e5c3af9f6545a7bcb
makysalu <44227511+makysalu@users.noreply.github.com>
2021-12-18 10:30:46 +0100

    Add files via upload

---
+++ FuncionesFlecha/funcionesFlecha.ts
@@ -0,0 +1,18 @@
+let suma = function(a:number, b:number){
+    return a+b;
+}
+
+console.log(suma(5,3))
+
+let sumaFlecha = (a:number, b:number) => a + b;
+
+console.log(sumaFlecha(3,2));
+
+var obtenerNombre = function(){
+    return "Diego Moreno";
+}
+console.log(obtenerNombre());
+
+let obtenerNombreFlecha = () => "Diego Moreno Alfaro"
+console.log(obtenerNombreFlecha());
+
