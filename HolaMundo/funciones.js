cf8714129d11c58b9bbe4c783885836938b23d21
makysalu <44227511+makysalu@users.noreply.github.com>
2021-12-18 10:31:23 +0100

    Add files via upload

---
+++ HolaMundo/funciones.js
@@ -0,0 +1,11 @@
+"use strict";
+let saludo = "Hola mundo desde TypeScript";
+let numero = 10;
+let cualquiera = "cuanquier dato";
+cualquiera = 10;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+}
+saludar();
---
+++ HolaMundo/funciones.ts
@@ -0,0 +1,16 @@
+let saludo: string = "Hola mundo desde TypeScript";
+let numero: number =10;
+
+let cualquiera: any = "cuanquier dato";
+cualquiera = 10;
+
+const PI: number = 3.14;
+
+function saludar():void{
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+    
+}
+
+saludar();
